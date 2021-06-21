5982442c891fa25a097251275370d7adefb695b6
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2021-06-21 03:10:25 -0300

    :bug: Fix: file export error

--- lib/thenafter_dart.dart
+++ lib/thenafter_dart.dart
@@ -3,5 +3,4 @@ library thenafter_dart;
 export 'src/controllers/bnf_parser.dart';
 export 'src/controllers/first_follow.dart';
 export 'src/python_synthatic_generator.dart';
-export 'src/util/input_iterator.dart';
 export 'src/util/types_util.dart';
