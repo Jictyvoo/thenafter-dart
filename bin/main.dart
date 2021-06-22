ade1b94a718b47c3688b0ea6a9547a944fb59786
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2021-06-22 16:33:20 -0300

    :8ball: Says: CLI Binary time `[Rewrote bin/main.dart]`

--- bin/main.dart
+++ bin/main.dart
@@ -1,43 +1,63 @@
 import 'dart:io';
 
+import 'package:args/args.dart';
 import 'package:thenafter_dart/thenafter_dart.dart';
 
-void main() {
-  final startTime = DateTime.now();
-  final parseResult = BNFParser().start(
-    File('grammar.grm').readAsBytesSync(),
-  );
-  final result = FirstFollow().start(
-    parseResult.productions,
-    parseResult.startSymbol.lexeme,
-  );
-  print(
-    'Finished in ${DateTime.now().difference(startTime).inMicroseconds} microseconds',
-  );
-  //print('${result.firstList}\n\n${result.followList}');
-  final generator = PythonGenerator();
-  final fileBuffer = StringBuffer();
-  final givenInfo = <String, String>{
-    'Identifier': 'TokenTypes.IDENTIFIER',
-    'DecLiteral': 'TokenTypes.NUMBER',
-    'OctLiteral': 'TokenTypes.NUMBER',
-    'HexLiteral': 'TokenTypes.NUMBER',
-    'FloatLiteral': 'TokenTypes.NUMBER',
-    'StringLiteral': 'TokenTypes.STRING',
-  };
-  generator.buildNeededImports(fileBuffer);
-  generator.buildTypeDeclarations(fileBuffer);
-  for (final currentProduction in parseResult.productions.entries) {
-    fileBuffer.writeln(
-      generator.buildFunction(
-        currentProduction.key,
-        currentProduction.value,
-        result.firstList,
-        givenInfo,
-      ),
-    );
-  }
+import 'util/syntactic_generator.dart';
 
-  final file = File('./synthatic_productions.log');
-  file.writeAsStringSync(fileBuffer.toString());
+ArgParser _configureArgParser() {
+  return ArgParser()
+    ..addCommand(
+      'generate',
+      ArgParser()
+        ..addFlag(
+          'productions',
+          abbr: 'p',
+          help: 'During code generation, '
+              'generate productions representation from .grm file',
+          defaultsTo: true,
+        )
+        ..addOption(
+          'language',
+          abbr: 'l',
+          help: 'Choose the output language for first, follow and productions',
+          defaultsTo: 'v',
+          mandatory: false,
+        )
+        ..addFlag(
+          'all',
+          abbr: 'a',
+          help: 'Parse all in current directory',
+          defaultsTo: false,
+        ),
+    )
+    ..addCommand('syntactic');
+}
+
+void main(List<String> args) {
+  // configure parser, parse args and get it's values
+  final argsParser = _configureArgParser();
+  final argResults = argsParser.parse(args);
+  for (final fileName in argResults.command?.rest ?? ['grammar.grm']) {
+    if (!fileName.endsWith('.grm')) {
+      continue;
+    }
+    final byteLines = File(fileName).readAsBytesSync();
+    final startTime = DateTime.now();
+    final parseResult = BNFParser().start(byteLines);
+    final result = FirstFollow().start(
+      parseResult.productions,
+      parseResult.startSymbol.lexeme,
+    );
+    print(
+      'Finished in ${DateTime.now().difference(startTime).inMicroseconds} '
+      'microseconds parse for file $fileName',
+    );
+    final commandName = argResults.command?.name ?? 'generate';
+    if (commandName == 'generate') {
+      print('${result.firstList}\n\n${result.followList}');
+    } else {
+      generateSyntacticFile(parseResult, result);
+    }
+  }
 }
---
+++ bin/util/syntactic_generator.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+import 'package:thenafter_dart/thenafter_dart.dart';
+
+void generateSyntacticFile(GrammarInformation parseResult, FirstFollowResult result) {
+  final generator = PythonGenerator();
+  final fileBuffer = StringBuffer();
+  final givenInfo = <String, String>{
+    'Identifier': 'TokenTypes.IDENTIFIER',
+    'DecLiteral': 'TokenTypes.NUMBER',
+    'OctLiteral': 'TokenTypes.NUMBER',
+    'HexLiteral': 'TokenTypes.NUMBER',
+    'FloatLiteral': 'TokenTypes.NUMBER',
+    'StringLiteral': 'TokenTypes.STRING',
+  };
+  generator.buildNeededImports(fileBuffer);
+  generator.buildTypeDeclarations(fileBuffer);
+  for (final currentProduction in parseResult.productions.entries) {
+    fileBuffer.writeln(
+      generator.buildFunction(
+        currentProduction.key,
+        currentProduction.value,
+        result.firstList,
+        givenInfo,
+      ),
+    );
+  }
+
+  final file = File('./syntactic_productions.log');
+  file.writeAsStringSync(fileBuffer.toString());
+}
