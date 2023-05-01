089310729935c5239aeba283807c622afd25240b
João Victor Oliveira Couto <commits@jictyvoo.dev>
2023-05-01 14:35:04 -0300

    :tea: test: Add more cases to AbstractSanitizer test

--- test/src/util/abstract_sanitizer_test.dart
+++ test/src/util/abstract_sanitizer_test.dart
@@ -1,37 +1,85 @@
 import 'package:test/test.dart';
 import 'package:thenafter_dart/src/util/abstract_sanitizer.dart';
 
-class _TestSubject with AbstractSanitizer {}
+class _TestSubject with AbstractSanitizer {
+  const _TestSubject();
+}
+
+class _TestNormalizeIdentifierCase {
+  final sanitizer = const _TestSubject();
+
+  final String input;
+  final String output;
+  final IdentifierFormat format;
+  final String Function(String)? unknownCharCallback;
+
+  const _TestNormalizeIdentifierCase({
+    required this.input,
+    required this.output,
+    required this.format,
+    this.unknownCharCallback,
+  });
+
+  void runTest() {
+    final testName = 'normalizeIdentifier `${format.name}'
+        '${unknownCharCallback == null ? "" : unknownCharCallback.toString()}`';
+    test(testName, () {
+      expect(
+        sanitizer.normalizeIdentifier(
+          input,
+          format: format,
+          unknownCharacterFallback: unknownCharCallback,
+        ),
+        equals(output),
+      );
+    });
+  }
+}
+
+String _as$(String _) {
+  return '\$';
+}
+
+String _asHexadecimal(String from) {
+  final buffer = StringBuffer();
+  for (final character in from.codeUnits) {
+    buffer.write(character.toRadixString(16));
+  }
+  return buffer.toString();
+}
 
 void main() {
-  final sanitizer = _TestSubject();
-  test('normalizeIdentifier camelCase', () {
-    const input1 = 'this_is-a_    test_identifier';
-    const output1 = 'thisIsATestIdentifier';
-    expect(
-      sanitizer.normalizeIdentifier(input1, format: IdentifierFormat.camelCase),
-      equals(output1),
-    );
-  });
+  const testCases = [
+    _TestNormalizeIdentifierCase(
+      input: 'this_is\t-a_   \n test_identifier',
+      output: 'thisIsATestIdentifier',
+      format: IdentifierFormat.camelCase,
+    ),
+    _TestNormalizeIdentifierCase(
+      input: 'this-   \t_ is -a_- --TEST_\n__identifier',
+      output: 'this_is_a_test_identifier',
+      format: IdentifierFormat.snakeCase,
+    ),
+    _TestNormalizeIdentifierCase(
+      input: 'this\r\n_is-a_Test_--\t\t\t--identifier',
+      output: 'ThisIsATestIdentifier',
+      format: IdentifierFormat.pascalCase,
+    ),
+    _TestNormalizeIdentifierCase(
+      input: 'this\tis-a <Test>_-__--<identifier\\%^',
+      output: 'ThisIsA\$Test\$\$identifier\$\$\$',
+      format: IdentifierFormat.pascalCase,
+      unknownCharCallback: _as$,
+    ),
+    _TestNormalizeIdentifierCase(
+      input: 'this\tis-a <Test>_-\$_--<identifier%^`',
+      output: 'ThisIsA3cTest3e243cidentifier255e60',
+      format: IdentifierFormat.pascalCase,
+      unknownCharCallback: _asHexadecimal,
+    ),
+  ];
 
-  test('normalizeIdentifier snakeCase', () {
-    const input2 = 'this-   _ is -a_- --TEST____identifier';
-    const output2 = 'this_is_a_test_identifier';
-    expect(
-      sanitizer.normalizeIdentifier(input2, format: IdentifierFormat.snakeCase),
-      equals(output2),
-    );
-  });
-
-  test('normalizeIdentifier pascalCase', () {
-    const input3 = 'this_is-a_Test_---_----__--identifier';
-    const output3 = 'ThisIsATestIdentifier';
-    expect(
-      sanitizer.normalizeIdentifier(
-        input3,
-        format: IdentifierFormat.pascalCase,
-      ),
-      equals(output3),
-    );
-  });
+  for (final tCase in testCases) {
+    tCase.runTest();
+  }
 }
